6428254f27098d2e21baaaf6d64287f3b4493676
Zenky <bzenky@gmail.com>
2023-07-15 17:41:56 -0300

    feat: radix collapsible

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -21,7 +21,7 @@ export function Player() {
             <Video />
           </div>
 
-          <aside className="w-80 absolute top-0 right-0 bottom-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
+          <aside className="w-80 absolute top-0 right-0 bottom-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-auto scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
             <Module moduleIndex={0} title="Desvendando o Github Student" lessonsAmount={12} />
             <Module moduleIndex={1} title="Desvendando o Github Student" lessonsAmount={12} />
             <Module moduleIndex={2} title="Desvendando o Github Student" lessonsAmount={12} />
